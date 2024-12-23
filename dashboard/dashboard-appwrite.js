f529e0b1e5d95b9d1fb66d2b146580717436cb39
sharmnten <sharmnten@gmail.com>
2024-12-23 00:55:16 +0000

    Fix dashboard

--- dashboard/dashboard-appwrite.js
+++ dashboard/dashboard-appwrite.js
@@ -2,12 +2,12 @@ const script = document.createElement('script');
 script.src = 'https://cdn.jsdelivr.net/npm/appwrite@16.0.2';
 
 script.onload=()=>{
-const client = new sdk.Client()
+const client = new Appwrite.Client()
     .setEndpoint('https://cloud.appwrite.io/v1')
     .setProject('67609b010021900fc6e6')
 
 
-const functions = new sdk.Functions(client)
+const functions = new Appwrite.Functions(client)
 
 const result =  functions.createExecution("loginPoints")
 
