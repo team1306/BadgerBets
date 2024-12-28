3c065a46f3c4e2a3f2ee0cde27ec37176f9c4c3b
sharmnten <sharmnten@gmail.com>
2024-12-28 10:37:20 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,39 +1,63 @@
 from appwrite.client import Client
 from appwrite.services.users import Users
+from appwrite.services.databases import Databases
 from appwrite.exception import AppwriteException
 import os
 
 # This Appwrite function will be executed every time your function is triggered
-def main(context):
+def main(context, userId, amount):
     # You can use the Appwrite SDK to interact with other services
     # For this example, we're using the Users service
     client = (
         Client()
-        .set_endpoint(os.environ["APPWRITE_FUNCTION_API_ENDPOINT"])
-        .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
-        .set_key(context.req.headers["x-appwrite-key"])
+        .set_endpoint('https://cloud.appwrite.io/v1')
+        .set_project('67609b010021900fc6e6')
+        .set_key('standard_a766a87da91bf274575dc294722839045769920a97a36eed295705391e817cad896c81e52c5dd654b6e7b3e6ec13bd16f3d607e416a543d5813d0aab7d7eae27af014a01aa74c503196056bd82eea708c844f1dcbba43f42d81a8a1c455727e6c93536fa6de367f8b92c23961ef1e9982f40c6b9caf1b46064515391dd1de308')
     )
     users = Users(client)
+    databases = Databases(client)
+    if(context == "get"):
+        getBucks(userId)
+    elif(context == "set"):
+        setBucks(userId, amount)
+    elif(context == "create"):
+        createBucks(userId, amount)
 
-    try:
-        response = users.list()
-        # Log messages and errors to the Appwrite Console
-        # These logs won't be seen by your end users
-        context.log("Total users: " + str(response["total"]))
-    except AppwriteException as err:
-        context.error("Could not list users: " + repr(err))
-
-    # The req object contains the request data
-    if context.req.path == "/ping":
-        # Use res object to respond with text(), json(), or binary()
-        # Don't forget to return a response!
-        return context.res.text("Pong")
-
-    return context.res.json(
-        {
-            "motto": "Build like a team of hundreds_",
-            "learn": "https://appwrite.io/docs",
-            "connect": "https://appwrite.io/discord",
-            "getInspired": "https://builtwith.appwrite.io",
-        }
+def getBucks(userId):
+    client = (
+        Client()
+        .set_endpoint('https://cloud.appwrite.io/v1')
+        .set_project('67609b010021900fc6e6')
+        .set_key('standard_a766a87da91bf274575dc294722839045769920a97a36eed295705391e817cad896c81e52c5dd654b6e7b3e6ec13bd16f3d607e416a543d5813d0aab7d7eae27af014a01aa74c503196056bd82eea708c844f1dcbba43f42d81a8a1c455727e6c93536fa6de367f8b92c23961ef1e9982f40c6b9caf1b46064515391dd1de308')
     )
+    databases = Databases(client)
+    result = databases.get_document('6760b9c20030df251f1c','badgerBucks', userId)
+    return result
+def setBucks(userId, amount):
+    client = (
+        Client()
+        .set_endpoint('https://cloud.appwrite.io/v1')
+        .set_project('67609b010021900fc6e6')
+        .set_key('standard_a766a87da91bf274575dc294722839045769920a97a36eed295705391e817cad896c81e52c5dd654b6e7b3e6ec13bd16f3d607e416a543d5813d0aab7d7eae27af014a01aa74c503196056bd82eea708c844f1dcbba43f42d81a8a1c455727e6c93536fa6de367f8b92c23961ef1e9982f40c6b9caf1b46064515391dd1de308')
+    )
+    databases = Databases(client)
+    document.update_document('6760b9c20030df251f1c','badgerBucks', userId, {'username': userId, 'badgerBucks': amount})
+
+    
+
+def createBucks(userId,amount):
+     client = (
+        Client()
+        .set_endpoint('https://cloud.appwrite.io/v1')
+        .set_project('67609b010021900fc6e6')
+        .set_key('standard_a766a87da91bf274575dc294722839045769920a97a36eed295705391e817cad896c81e52c5dd654b6e7b3e6ec13bd16f3d607e416a543d5813d0aab7d7eae27af014a01aa74c503196056bd82eea708c844f1dcbba43f42d81a8a1c455727e6c93536fa6de367f8b92c23961ef1e9982f40c6b9caf1b46064515391dd1de308')
+    )
+    databases = Databases(client)
+    databases.create_document('6760b9c20030df251f1c','badgerBucks', userId, {'username': userId, 'badgerBucks': amount})
+
+    
+
+
+
+
+        
