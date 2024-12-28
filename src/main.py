067ebd696ba573b0cc2b12fe1ad5446a8fa10af2
sharmnten <sharmnten@gmail.com>
2024-12-28 17:17:14 +0000

    fixed python script

--- src/main.py
+++ src/main.py
@@ -17,43 +17,20 @@ def main(context, userId, amount):
     users = Users(client)
     databases = Databases(client)
     if(context == "get"):
-        getBucks(userId)
+        result = databases.get_document('6760b9c20030df251f1c','badgerBucks', userId)
+        return result['badgerBucks']
     elif(context == "set"):
-        setBucks(userId, amount)
+         databases.update_document('6760b9c20030df251f1c','badgerBucks', userId, {'username': userId, 'badgerBucks': amount})
     elif(context == "create"):
-        createBucks(userId, amount)
+        databases.create_document('6760b9c20030df251f1c','badgerBucks', userId, {'username': userId, 'badgerBucks': amount})
 
-def getBucks(userId):
-    client = (
-        Client()
-        .set_endpoint('https://cloud.appwrite.io/v1')
-        .set_project('67609b010021900fc6e6')
-        .set_key('standard_a766a87da91bf274575dc294722839045769920a97a36eed295705391e817cad896c81e52c5dd654b6e7b3e6ec13bd16f3d607e416a543d5813d0aab7d7eae27af014a01aa74c503196056bd82eea708c844f1dcbba43f42d81a8a1c455727e6c93536fa6de367f8b92c23961ef1e9982f40c6b9caf1b46064515391dd1de308')
-    )
-    databases = Databases(client)
-    result = databases.get_document('6760b9c20030df251f1c','badgerBucks', userId)
-    return result
-def setBucks(userId, amount):
-    client = (
-        Client()
-        .set_endpoint('https://cloud.appwrite.io/v1')
-        .set_project('67609b010021900fc6e6')
-        .set_key('standard_a766a87da91bf274575dc294722839045769920a97a36eed295705391e817cad896c81e52c5dd654b6e7b3e6ec13bd16f3d607e416a543d5813d0aab7d7eae27af014a01aa74c503196056bd82eea708c844f1dcbba43f42d81a8a1c455727e6c93536fa6de367f8b92c23961ef1e9982f40c6b9caf1b46064515391dd1de308')
-    )
-    databases = Databases(client)
-    document.update_document('6760b9c20030df251f1c','badgerBucks', userId, {'username': userId, 'badgerBucks': amount})
+
+
+   
 
     
 
-def createBucks(userId,amount):
-     client = (
-        Client()
-        .set_endpoint('https://cloud.appwrite.io/v1')
-        .set_project('67609b010021900fc6e6')
-        .set_key('standard_a766a87da91bf274575dc294722839045769920a97a36eed295705391e817cad896c81e52c5dd654b6e7b3e6ec13bd16f3d607e416a543d5813d0aab7d7eae27af014a01aa74c503196056bd82eea708c844f1dcbba43f42d81a8a1c455727e6c93536fa6de367f8b92c23961ef1e9982f40c6b9caf1b46064515391dd1de308')
-    )
-    databases = Databases(client)
-    databases.create_document('6760b9c20030df251f1c','badgerBucks', userId, {'username': userId, 'badgerBucks': amount})
+    
 
     
 
