e1e51831641753c73642fe3e8bc70a0d8800287d
sharmnten <sharmnten@gmail.com>
2024-12-29 15:58:45 +0000

    Added secrets for security

--- src/main.py
+++ src/main.py
@@ -3,13 +3,20 @@ from appwrite.client import Client
 from appwrite.services.users import Users
 from appwrite.services.databases import Databases
 from appwrite.exception import AppwriteException
+import os
+
+
+endpoint = os.getenv("API_ENDPOINT")
+key = os.getenv("API_KEY")
+project = os.getenv("PROJECT_ID")
+
 
 def main(context, userId, amount):
     client = (
         Client()
-        .set_endpoint('https://cloud.appwrite.io/v1')
-        .set_project('67609b010021900fc6e6')
-        .set_key('standard_a766a87da91bf274575dc294722839045769920a97a36eed295705391e817cad896c81e52c5dd654b6e7b3e6ec13bd16f3d607e416a543d5813d0aab7d7eae27af014a01aa74c503196056bd82eea708c844f1dcbba43f42d81a8a1c455727e6c93536fa6de367f8b92c23961ef1e9982f40c6b9caf1b46064515391dd1de308')
+        .set_endpoint(endpoint)
+        .set_project(key)
+        .set_key(project)
     )
     users = Users(client)
     databases = Databases(client)
