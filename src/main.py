d98990caa89b504619c45d1cb02aef1badb7e324
sharmnten <sharmnten@gmail.com>
2024-12-29 12:26:03 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,7 +6,6 @@ import os
 key = os.API_KEY
 # This Appwrite function will be executed every time your function is triggered
 def main(context,function, userId, amount):
-    return 1
     type = context.req.query['type']
     # You can use the Appwrite SDK to interact with other services
     # For this example, we're using the Users service
@@ -14,14 +13,14 @@ def main(context,function, userId, amount):
         Client()
         .set_endpoint('https://cloud.appwrite.io/v1')
         .set_project('67609b010021900fc6e6')
-        .set_key(key)
+        .set_key("standard_a766a87da91bf274575dc294722839045769920a97a36eed295705391e817cad896c81e52c5dd654b6e7b3e6ec13bd16f3d607e416a543d5813d0aab7d7eae27af014a01aa74c503196056bd82eea708c844f1dcbba43f42d81a8a1c455727e6c93536fa6de367f8b92c23961ef1e9982f40c6b9caf1b46064515391dd1de308")
     )
     users = Users(client)
     databases = Databases(client)
     if(function == "get"):
         result = databases.get_document('6760b9c20030df251f1c','badgerBucks', userId)
-        result = result.['badgerBucks']
-        return context.res.text(results)
+        result = result['badgerBucks']
+        return context.res.text(result)
     elif(function == "set"):
          databases.update_document('6760b9c20030df251f1c','badgerBucks', userId, {'username': userId, 'badgerBucks': amount})
     elif(function == "create"):
