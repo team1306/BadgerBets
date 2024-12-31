b85b9c0caa4c8ab0275f006978723e2fe99e7e26
sharmnten <sharmnten@gmail.com>
2024-12-31 16:49:16 +0000

    added creation of a document when a user signs up.

--- signup/signup-appwrite.js
+++ signup/signup-appwrite.js
@@ -5,6 +5,7 @@ script.onload=()=>{
   .setEndpoint('https://cloud.appwrite.io/v1') // Replace with your Appwrite server URL
   .setProject('67609b010021900fc6e6'); 
   const account = new Appwrite.Account(client);
+  const functions = new Appwrite.Account(client);
   //code for sign up page
   document.getElementById('signup-form').addEventListener('submit',async (event)=>{
     event.preventDefault();
@@ -28,7 +29,27 @@ script.onload=()=>{
     }
     try {
       // Try to create a session with the provided email and password
-      const session = await account.create(username, email, password,name); // Correct method for creating session
+      const session = await account.create(username, email, password,name);
+      
+      try {
+        const user = await account.get();
+        const userId = user.$id;
+        const functionId = '6770291b00171ec2611b'; // Replace with your function ID
+        const parameters = JSON.stringify({
+            action: 'create',
+            userId: userId,
+            badgerBucks: 100
+        });
+
+        // Execute the function
+        let result = await functions.createExecution(functionId, parameters);
+        result = JSON.parse(result.responseBody)
+        currentBadgerBucks = result.badgerBucks
+        console.log('Your badgerBucks:', currentBadgerBucks);
+    } catch (error) {
+        alert("There was an error");
+        console.error(error);
+    }// Correct method for creating session
       
       
       // Redirect to dashboard or another page
