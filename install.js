3b46af56986eddff1095ed8a7b18d2d00d8fbadc
Ari Mehta <imakecoolstuff@icloud.com>
2025-02-15 13:47:56 -0600

    install button

---
+++ install.js
@@ -0,0 +1,24 @@
+installButton.addEventListener("click", async () => {
+    if (!installPrompt) {
+        return;
+    }
+    const result = await installPrompt.prompt();
+    console.log(`Install prompt was: ${result.outcome}`);
+    disableInAppInstallPrompt();
+});
+
+function disableInAppInstallPrompt() {
+    installPrompt = null;
+    installButton.setAttribute("hidden", "");
+}
+
+
+
+window.addEventListener("appinstalled", () => {
+    disableInAppInstallPrompt();
+  });
+  
+  function disableInAppInstallPrompt() {
+    installPrompt = null;
+    installButton.setAttribute("hidden", "");
+  }
