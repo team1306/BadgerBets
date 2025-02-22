3397603170b54a3bd3e59e6982b57c78467139ad
Neal Sharma <sharmnten@gmail.com>
2025-02-22 14:43:43 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -7,8 +7,7 @@ def main(context):
     client = Client()
     client.set_endpoint('https://cloud.appwrite.io/v1')  # Replace with your Appwrite endpoint
     client.set_project('67609b010021900fc6e6')  # Replace with your project ID
-    client.set_key('standard_a766a87da91bf274575dc294722839045769920a97a36eed295705391e817cad896c81e52c5dd654b6e7b3e6ec13bd16f3d607e416a543d5813d0aab7d7eae27af014a01aa74c503196056bd82eea708c844f1dcbba43f42d81a8a1c455727e6c93536fa6de367f8b92c23961ef1e9982f40c6b9caf1b46064515391dd1de308')  # Replace with your API key (or use OAuth)
-
+    
     # Initialize Appwrite Databases service
     databases = Databases(client)
     parameters = context.req.body_json
