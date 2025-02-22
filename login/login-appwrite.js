8f199632bf8d4b0ca4a1431b404608b427d496a0
MrAtomicBombHead <142016095+MrAtomicBombHead@users.noreply.github.com>
2025-02-22 20:29:46 +0000

    skips login page if session is saved

--- login/login-appwrite.js
+++ login/login-appwrite.js
@@ -1,40 +1,45 @@
 import { Client, Account } from "https://esm.sh/appwrite@14.0.1";
 import {login} from "./login-helper.js";
-  const client = new Client()
-    .setEndpoint('https://cloud.appwrite.io/v1') // Replace with your Appwrite server URL
-    .setProject('67609b010021900fc6e6');
+const client = new Client()
+  .setEndpoint('https://cloud.appwrite.io/v1') // Replace with your Appwrite server URL
+  .setProject('67609b010021900fc6e6');
 
-  const account = new Account(client);
+const account = new Account(client);
 
-  const loginForm = document.getElementById('login-form');
-  if (loginForm) {
-    loginForm.addEventListener('submit', async (event) => {
-      event.preventDefault();
 
-      const emailInput = document.getElementById('login-email');
-      const passwordInput = document.getElementById('login-password');
+if (localStorage.getItem('session') != null) window.location.href = '../dashboard/index.html';
+console.log(localStorage.getItem('session'));
 
-      if (!emailInput || !passwordInput) {
-        console.error('Login form inputs not found.');
-        return;
-      }
 
-      let email = emailInput.value.trim();
-      const password = passwordInput.value.trim();
+const loginForm = document.getElementById('login-form');
+if (loginForm) {
+  loginForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
 
-      if (!email || !password) {
-        alert('Please fill in both fields.');
-        return;
-      }
+    const emailInput = document.getElementById('login-email');
+    const passwordInput = document.getElementById('login-password');
 
-      const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-      if (!emailRegex.test(email)) {
-        alert('Invalid email format.');
-        return;
-      }
-      await login(account, email, password);
-    });
-  } else {
-    console.error('Login form not found.');
-  }
+    if (!emailInput || !passwordInput) {
+      console.error('Login form inputs not found.');
+      return;
+    }
+
+    let email = emailInput.value.trim();
+    const password = passwordInput.value.trim();
+
+    if (!email || !password) {
+      alert('Please fill in both fields.');
+      return;
+    }
+
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    if (!emailRegex.test(email)) {
+      alert('Invalid email format.');
+      return;
+    }
+    await login(account, email, password);
+  });
+} else {
+  console.error('Login form not found.');
+}
 
