6ee7b2edbf016e1d40012a1ce8b68712edfb766a
Neal Sharma <sharmnten@gmail.com>
2025-02-22 22:33:00 +0000

    Added the qr code generator. Integrate this how you wish.

--- qrcodemaker.js
+++ qrcodemaker.js
@@ -1,32 +1,28 @@
-import QRCode from 'qrcode';
+import QrCreator from 'https://cdn.jsdelivr.net/npm/qr-creator/dist/qr-creator.es6.min.js';
+let availableSettings = ['text', 'radius', 'ecLevel', 'fill', 'background', 'size'];
 
-export default {
-  data() {
-    return {
-      inputData: ''
-    };
-  },
-  methods: {
-    // Triggered when the button is clicked
-    onGenerateQRCode() {
-      this.generateQRCode(this.inputData);
-    },
-    
-    // Generate QR code from provided data
-    generateQRCode(data) {
-      // Clear any previous QR code
-      document.getElementById('qrcode').innerHTML = '';
-
-      // Generate a new QR code if data is not empty
-      if (data) {
-        QRCode.toCanvas(document.getElementById('qrcode'), data, function (error) {
-          if (error) console.error(error);
-          console.log('QR Code generated!');
-        });
-      } else {
-        alert('Please enter some text or a URL.');
-      }
-    }
+function readSettings() {
+  let settings = {}, el;
+  for (let setting of availableSettings) {
+	settings[setting] = document.querySelector('#'+setting).value;
   }
-};
+  if (document.querySelector('#transparent').checked) {
+	settings.background = null;
+  }
+  return settings;
+}
 
+function renderQrCode() {
+  let time = new Date(),
+	  container = document.querySelector('#qr-code'),
+	  settings = readSettings();
+  container.innerHTML = '';
+  QrCreator.render(settings, container);
+  document.querySelector('#stats').innerText = 'Rendered in ' + (new Date()-time) + 'ms';
+  document.querySelector('#json').innerText = JSON.stringify(settings, null, 2);
+}
+
+for (let input of document.querySelectorAll('input, select')) {
+  input.addEventListener('change', renderQrCode);
+}
+renderQrCode();
