42416de117def89cd1de9f9a0e1d716764cbdb90
MrAtomicBombHead <142016095+MrAtomicBombHead@users.noreply.github.com>
2025-04-02 00:30:59 +0000

    bug fix. Thanks chatgpt

--- gamble/script.js
+++ gamble/script.js
@@ -294,9 +294,8 @@ async function getCurrentTime() {
 async function getMatchIds() {
     const result = await executeFunction("getMatches", Appwrite.ExecutionMethod.GET);
     const matches = JSON.parse(result.responseBody)[0].Schedule;
-    console.log(JSON.parse(result.responseBody)[1]);
     console.log(JSON.parse(result.responseBody)[1].Schedule);
-    matches.concat(JSON.parse(result.responseBody)[1].Schedule);
+    matches = matches.concat(JSON.parse(result.responseBody)[1].Schedule);
 
     console.log(matches);
 
